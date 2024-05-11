b3160c3cd01316024770c985c18ca52834759c66
Rishabh Indoria <rishabhindoria@rishabhs-mbp.mynetworksettings.com>
2024-05-11 15:21:01 -0400

    final

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -1,78 +1,3 @@
-# import tensorflow as tf
-# from pathlib import Path
-# import mlflow
-# import mlflow.keras
-# from urllib.parse import urlparse
-# from cnnClassifier.entity.config_entity import EvaluationConfig
-# from cnnClassifier.utils.common import read_yaml, create_directories,save_json
-
-
-# class Evaluation:
-#     def __init__(self, config: EvaluationConfig):
-#         self.config = config
-
-    
-#     def _valid_generator(self):
-
-#         datagenerator_kwargs = dict(
-#             rescale = 1./255,
-#             validation_split=0.30
-#         )
-
-#         dataflow_kwargs = dict(
-#             target_size=self.config.params_image_size[:-1],
-#             batch_size=self.config.params_batch_size,
-#             interpolation="bilinear"
-#         )
-
-#         valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
-#             **datagenerator_kwargs
-#         )
-
-#         self.valid_generator = valid_datagenerator.flow_from_directory(
-#             directory=self.config.training_data,
-#             subset="validation",
-#             shuffle=False,
-#             **dataflow_kwargs
-#         )
-
-
-#     @staticmethod
-#     def load_model(path: Path) -> tf.keras.Model:
-#         return tf.keras.models.load_model(path)
-    
-
-#     def evaluation(self):
-#         self.model = self.load_model(self.config.path_of_model)
-#         self._valid_generator()
-#         self.score = self.model.evaluate(self.valid_generator)
-#         self.save_score()
-
-#     def save_score(self):
-#         scores = {"loss": self.score[0], "accuracy": self.score[1]}
-#         save_json(path=Path("scores.json"), data=scores)
-
-    
-#     def log_into_mlflow(self):
-#         mlflow.set_registry_uri(self.config.mlflow_uri)
-#         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        
-#         with mlflow.start_run():
-#             mlflow.log_params(self.config.all_params)
-#             mlflow.log_metrics(
-#                 {"loss": self.score[0], "accuracy": self.score[1]}
-#             )
-#             # Model registry does not work with file store
-#             if tracking_url_type_store != "file":
-
-#                 # Register the model
-#                 # There are other ways to use the Model Registry, which depends on the use case,
-#                 # please refer to the doc for more information:
-#                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-#                 mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
-#             else:
-#                 mlflow.keras.log_model(self.model, "model")
-
 import tensorflow as tf
 from cnnClassifier.entity.config_entity import EvaluationConfig
 from pathlib import Path
@@ -83,6 +8,9 @@ from urllib.parse import urlparse
 class Evaluation:
     def __init__(self, config: EvaluationConfig):
         self.config = config
+        self.model = None
+        self.valid_generator = None
+        self.score = None
 
     def _valid_generator(self):
         dataflow_kwargs = {
@@ -126,13 +54,25 @@ class Evaluation:
         save_json(path=Path("scores.json"), data=scores)
 
     def log_into_mlflow(self):
-        mlflow.set_registry_uri(self.config.mlflow_uri)
+        mlflow.set_tracking_uri(self.config.mlflow_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-        with mlflow.start_run():
-            mlflow.log_params(self.config.all_params)
-            mlflow.log_metrics({"loss": self.score[0], "accuracy": self.score[1]})
+        print("MLflow Tracking URI:", mlflow.get_tracking_uri())
+        print("MLflow Registry URI:", mlflow.get_registry_uri())
+        
+        with mlflow.start_run(run_name="Model Evaluation") as run:
+            # Log parameters
+            for param, value in self.config.all_params.items():
+                mlflow.log_param(param, value)
+
+            # Log evaluation metrics
+            mlflow.log_metric("loss", self.score[0])
+            mlflow.log_metric("accuracy", self.score[1])
+
+            # Log the model
             if tracking_url_type_store != "file":
                 mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
             else:
                 mlflow.keras.log_model(self.model, "model")
+
+            mlflow.end_run()
